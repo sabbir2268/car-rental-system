98f51a7cbd11217d45d958c4c2669073fcdd3ea2
Md Sabbir Rahman <mdsabbirrahman2268@gmail.com>
2026-05-17 22:11:18 +0600

    updated server/index.js for deploy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 require("dotenv").config();
 
 const authRoutes = require("./routes/auth");
