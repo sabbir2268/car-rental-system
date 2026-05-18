41fb3621efaad47cb715f56b088841effecc0a30
Md Sabbir Rahman <mdsabbirrahman2268@gmail.com>
2026-05-18 08:55:20 +0600

    trying to deploy

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_URL || "/api";
+
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL,
   withCredentials: true,
 });
 
